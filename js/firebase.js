5fc202747897e20ce41ea2c04f327d7a11697820
Leandro <leandroquiroga9514@gmail.com>
2021-06-07 15:42:18 -0300

    firebase

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+  // Configuracion Firebase
+  var firebaseConfig = {
+    apiKey: "AIzaSyC_FxM_wArZokcJFbMcqXiaOM9oK4tgbyA",
+    authDomain: "my-team-page-f87b9.firebaseapp.com",
+    projectId: "my-team-page-f87b9",
+    storageBucket: "my-team-page-f87b9.appspot.com",
+    messagingSenderId: "539088801161",
+    appId: "1:539088801161:web:17bef3481c51aea8c1e558"
+  };
+  // inicializa Firebase
+  firebase.initializeApp(firebaseConfig);
